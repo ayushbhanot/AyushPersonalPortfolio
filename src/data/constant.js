5c5cd700a406d12ae5922fece29021231c3e53ee
ayushbhanot <ayushbhanot1010@gmail.com>
2025-07-13 18:26:51 -0400

    Added Mobile App HealthCare Project

--- src/data/constant.js
+++ src/data/constant.js
@@ -152,18 +152,17 @@ export const experiences = [
       title: "Data HealthCare App",
       description:
   "Developed a cross-platform mobile app for DataCare LLC to streamline patient registration and real-time data collection at healthcare camps. Built with React Native and Expo, the app features dynamic medical forms, photo ID uploads, JWT-based authentication, and real-time syncing with a secure backend powered by Node.js, Express, and YugabyteDB on GCP.",
-      image:
-        "https://raw.githubusercontent.com/ayushbhanot/HealthCareDataAppPersonal/main/assets/gif/home.gif",
+  image: "https://raw.githubusercontent.com/ayushbhanot/HealthCareDataApp/ayush/assets/gif/home.gif",
       tags: [
         "React Native",
         "Node.js",
         "Express.js",
         "YugabyteDB",
-        "JWT Auth",
-        "Multer",
+        "JWT",
         "Mobile App",
         "GCP"
-      ],
+      ]
+      ,
       category: "mobile app",
       github: "https://github.com/ayushbhanot/HealthCareDataAppPersonal",
       // webapp: "", // if deployed mobile app store link or APK is available
